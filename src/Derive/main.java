9a2c00bf18de38f5bff48800210ae1b0a57fd9e7
Ashwin Valento <ashwinvalento@gmail.com>
2014-05-03 02:15:59 +0530

    Online Activation Added

--- src/Derive/main.java
+++ src/Derive/main.java
@@ -7,6 +7,8 @@ package Derive;
 
 import Client.ClientAuthentication;
 import Server.ServerAuthentication;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,18 +17,41 @@ import Server.ServerAuthentication;
 public class main {
 
     public static void main(String args[]) {
+
+        String url = "http://localhost:8080/onlineauthenticate.jsp";
+        System.out.println("\n\n----------------------------------\n\n");
+
         //step 1
+        System.out.println("Step 1 :");
         Client.ClientAuthentication cA = new ClientAuthentication("Hello world1");
         String token = cA.getToken();
         System.out.println("token generated == " + token);
-        
+
         //Step 2
-        Server.ServerAuthentication sa = new ServerAuthentication("Hello world1");
+        System.out.println("\nStep 2 :");
+        ServerAuthentication sa = new ServerAuthentication("Hello world1");
         String License = sa.generateLicense(token);
         System.out.println("Licence key Generated == " + License);
-        
+
         //Step 3
-        cA.validate(License);
+        System.out.println("\nStep 3 :");
+        if(cA.validate(License)){
+            System.out.println("*******Validation Successfull******");
+        }else{
+            System.out.println("xxxxxx*Validation Not Successfull*xxxxxx");
+        }
+
+        //online activation
+        System.out.println("\n---------Online Activation------\n");
+        String onlineLicense;
+        try {
+
+            onlineLicense = cA.getLicenseOnline(url);
+            System.out.println("Online Response code = "+cA.getResponseCode());
+            System.out.println("Online License  = "+onlineLicense);
+        } catch (Exception ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
